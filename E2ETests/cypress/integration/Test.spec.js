e462e9ab2e65e836c0909ee8950a7b990eb5f320
SofiaLindgren <sofia.lindgren@iths.se>
2021-01-27 22:22:17 +0100

    Added working e2e tests

---
+++ E2ETests/cypress/integration/Test.spec.js
@@ -0,0 +1,21 @@
+﻿describe('bug tracker app', () => {
+
+    it('Open the app', () => {
+        cy.visit('http://localhost:49164');
+    })
+
+    it('add a bug', () => {
+        cy.get('[data-cy=Add-bug]').click()
+        cy.get('[data-cy=bug-description]').type('I am a bug')
+        cy.get('[data-cy=create-button]').click()
+    }) 
+
+    it('remove the bug', () => {
+        cy.contains('I am a bug').parent('tr').within(() => {
+            cy.get('td').eq(1).contains('Delete').click()
+        })
+        cy.get('[data-cy=delete-bug]').click()
+    })
+})
+
+
